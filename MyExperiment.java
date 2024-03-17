57f5699b534768482c873257e39b34bdf3feb760
kon104 <toshiharu@MacBookAir2023.local>
2024-03-17 21:37:29 +0900

    improved the structure of methods

--- MyExperiment.java
+++ MyExperiment.java
@@ -9,97 +9,84 @@ import java.util.LinkedList;
 
 class MyExperiment {
 	public static void main(String[] args) {
-		if (true) readFile();
-		if (true) resizeArray1();
-		if (true) resizeArray2();
-	}
 
-	private static void printProcessingTime(long startTime, long endTime) {
-		double diff = (endTime - startTime) / 1000000000.0;
-		System.out.printf("処理時間: %.10f nano second in %s()", diff, Thread.currentThread().getStackTrace()[2].getMethodName());
-		System.err.println();
-		System.out.println(String.join("", Collections.nCopies(60, "-")));
-	}
-
-	private static void readFile() {
-		long startTime = System.nanoTime();
-
-		String path = "./java-study/input001.txt";
-		LinkedList<String> lines = new LinkedList<>();
-		try {
-			FileInputStream fis = new FileInputStream(path);
-			InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
-			BufferedReader bf = new BufferedReader(isr);
-			String line;
-		    while((line = bf.readLine()) != null) {
-				lines.add(line);
+		Runnable readFile = () -> {
+			String path = "./java-study/input001.txt";
+			LinkedList<String> lines = new LinkedList<>();
+			try {
+				FileInputStream fis = new FileInputStream(path);
+				InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
+				BufferedReader bf = new BufferedReader(isr);
+				String line;
+				while((line = bf.readLine()) != null) {
+					lines.add(line);
+				}
+				bf.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
 			}
-		    bf.close();
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		}
+	
+			for(String l: lines){
+				System.out.println(l);
+			}				
+		};
 
-		for(String l: lines){
-			System.out.println(l);
-		}
-
-		long entTime = System.nanoTime();
-		printProcessingTime(startTime, entTime);
-	}
-
-	private static void resizeArray1() {
-		long startTime = System.nanoTime();
-
-		int src[] = new int[15];
-		for (int i = 0; i < src.length; i++) {
-			src[i] = (i + 1);
-		}
-
-		int tmp[] = new int[src.length];
-		int j = 0;
-		for (int i: src) {
-			if ((i % 3) != 0) {
-				tmp[j++] = i;
+		Runnable resizeArrayBySystem = () -> {
+			int src[] = new int[15];
+			for (int i = 0; i < src.length; i++) {
+				src[i] = (i + 1);
 			}
-		}
-
-		int dst[] = new int[j];
-		System.arraycopy(tmp, 0, dst, 0, dst.length);
-
-		System.out.printf("There are %s values from %s to %s.", dst.length, dst[0], dst[dst.length - 1]);
-		System.out.println("");
-
-		long endTime = System.nanoTime();
-		printProcessingTime(startTime, endTime);
-
-
-	}
-
-	private static void resizeArray2() {
-		long startTime = System.nanoTime();
-
-		int src[] = new int[15];
-		for (int i = 0; i < src.length; i++) {
-			src[i] = (i + 1);
-		}
-
-		int tmp[] = new int[src.length];
-		int j = 0;
-		for (int i: src) {
-			if ((i % 3) != 0) {
-				tmp[j++] = i;
+	
+			int tmp[] = new int[src.length];
+			int j = 0;
+			for (int i: src) {
+				if ((i % 3) != 0) {
+					tmp[j++] = i;
+				}
 			}
-		}
+	
+			int dst[] = new int[j];
+			System.arraycopy(tmp, 0, dst, 0, dst.length);
+	
+			System.out.printf("There are %s values from %s to %s.", dst.length, dst[0], dst[dst.length - 1]);
+			System.out.println("");
+		};
 
-		int dst[] = Arrays.copyOf(tmp, j);
+		Runnable resizeArrayByArrays = () -> {
+			int src[] = new int[15];
+			for (int i = 0; i < src.length; i++) {
+				src[i] = (i + 1);
+			}
+	
+			int tmp[] = new int[src.length];
+			int j = 0;
+			for (int i: src) {
+				if ((i % 3) != 0) {
+					tmp[j++] = i;
+				}
+			}
+	
+			int dst[] = Arrays.copyOf(tmp, j);
+	
+			System.out.printf("There are %s values from %s to %s.", dst.length, dst[0], dst[dst.length - 1]);
+			System.out.println("");
+		};
 
-		System.out.printf("There are %s values from %s to %s.", dst.length, dst[0], dst[dst.length - 1]);
-		System.out.println("");
-
-		long endTime = System.nanoTime();
-		printProcessingTime(startTime, endTime);
+		if (true) calcProcessingTime(readFile, "readFile");
+		if (true) calcProcessingTime(resizeArrayBySystem, "resizeArrayBySystem");
+		if (true) calcProcessingTime(resizeArrayByArrays, "resizeArrayByArrays");
 	}
 
-
+	private static void calcProcessingTime(Runnable runnable, String name) {
+		long startTimeMs = System.currentTimeMillis();
+		long startTimeNs = System.nanoTime();
+		runnable.run();
+		long endTimeMs = System.currentTimeMillis();
+		long endTimeNs = System.nanoTime();
+		long diffMs = endTimeMs - startTimeMs;
+		double diffNs = (endTimeNs - startTimeNs) / 1000000000.0;
+		System.out.printf("処理時間: %d msec (%.10f sec) in %s()", diffMs, diffNs, name);
+		System.out.println();
+	}
 }
 
