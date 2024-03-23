2820705212aee510e9dd1df15b0b34b135578dfa
kon104 <toshiharu@MacBookAir2023.local>
2024-03-23 17:07:38 +0900

    improved the source code

--- ExpAddingGameInWonderland.java
+++ ExpAddingGameInWonderland.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ExpAddingGameInWonderland extends ExpBase {
@@ -7,40 +8,49 @@ public class ExpAddingGameInWonderland extends ExpBase {
 		long startTimeNs = System.nanoTime();		
 
 		for (int test_case = 1; sc.hasNext(); test_case++) {
-			int num = sc.nextInt();
+			String line = sc.nextLine();
+			char[] src = line.toCharArray();
 			int count = 0;
-			while(num >= 10) {
+
+//			System.out.println("\t" + line);
+			while (src.length > 1) {
 				count++;
-//				System.out.println("\t" + num);
-				int d1 = num % 10;
-				int d2 = ((int)(num / 10)) % 10;
-				if (num >= 1000) {
-					int d3 = ((int)(num / 100)) % 10;
-					int d4 = (int)(num / 1000);
-					int x1 = (d4 + d3) * 100 + d2 * 10 + d1;
-					int x2 = d4 * ((d3 + d2) < 10 ? 100 : 1000) + (d3 + d2) * 10 + d1;
-					int x3 = d4 * ((d2 + d1) < 10 ? 100 : 1000) + d3 * ((d2 + d1) < 10 ? 10 : 100) + (d2 + d1);
-					if ((x1 > x2) && (x1 > x3)) {
-						num = x1;
-					} else if (x2 > x3) {
-						num = x2;
-					} else {
-						num = x3;
-					}
-				} else if (num >= 100) {
-					int d3 = ((int)(num / 100)) % 10;
-					int x1 = (d3 + d2) * 10 + d1;
-					int x2 = d3 * ((d2 + d1) < 10 ? 10 : 100) + (d2 + d1);
-					if (x1 > x2) {
-						num = x1;
-					} else {
-						num = x2;
-					}
-				} else {
-					num = d2 + d1;
+
+				int[] pair = new int[src.length - 1];
+				for (int i = 0; i < src.length - 1; i++) {
+					pair[i] = (src[i] - 48) + (src[i + 1] - 48);
 				}
+				int maxVal = 0;
+				int maxPos = 0;
+				boolean isOver10 = false;
+				for (int i = 0; i < pair.length; i++) {
+					if (pair[i] > maxVal) {
+						maxVal = pair[i];
+						maxPos = i;
+						if (maxVal >= 10) {
+							isOver10 = true;
+						}
+					}
+				}
+				char[] dst = new char[(isOver10 ? src.length : (src.length - 1))];
+				for (int i = 0, j = 0; i < dst.length; i++, j++) {
+					if (i == maxPos) {
+						if (isOver10) {
+							dst[i] = (char)(((int)(maxVal / 10)) + 48);
+							i++;
+							dst[i] = (char)((maxVal % 10) + 48);
+						} else {
+							dst[i] = (char)(maxVal + 48);
+						}
+						j++;
+					} else {
+						dst[i] = src[j];
+					}
+				}
+//				System.out.println("\t\tnxt=" + String.valueOf(dst));
+				src = Arrays.copyOf(dst, dst.length);
 			}
-//			System.out.println("\t" + num);
+
 			System.out.println("#" + test_case + " " + ((count % 2) == 0 ? "B" : "A" ));
 		}
 		sc.close();
