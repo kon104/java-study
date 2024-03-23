a8520349d6d74bb91ff2cb8d299b00bbc2f16291
kon104 <toshiharu@MacBookAir2023.local>
2024-03-23 17:46:08 +0900

    improved the src

--- ExpAddingGameInWonderland.java
+++ ExpAddingGameInWonderland.java
@@ -12,42 +12,14 @@ public class ExpAddingGameInWonderland extends ExpBase {
 			char[] src = line.toCharArray();
 			int count = 0;
 
-//			System.out.println("\t" + line);
+//			System.out.println("\t(" + ((count % 2) == 0 ? 'A' : 'B') + ")curr=" + line);
 			while (src.length > 1) {
 				count++;
 
-				int[] pair = new int[src.length - 1];
-				for (int i = 0; i < src.length - 1; i++) {
-					pair[i] = (src[i] - 48) + (src[i + 1] - 48);
-				}
-				int maxVal = 0;
-				int maxPos = 0;
-				boolean isOver10 = false;
-				for (int i = 0; i < pair.length; i++) {
-					if (pair[i] > maxVal) {
-						maxVal = pair[i];
-						maxPos = i;
-						if (maxVal >= 10) {
-							isOver10 = true;
-						}
-					}
-				}
-				char[] dst = new char[(isOver10 ? src.length : (src.length - 1))];
-				for (int i = 0, j = 0; i < dst.length; i++, j++) {
-					if (i == maxPos) {
-						if (isOver10) {
-							dst[i] = (char)(((int)(maxVal / 10)) + 48);
-							i++;
-							dst[i] = (char)((maxVal % 10) + 48);
-						} else {
-							dst[i] = (char)(maxVal + 48);
-						}
-						j++;
-					} else {
-						dst[i] = src[j];
-					}
-				}
-//				System.out.println("\t\tnxt=" + String.valueOf(dst));
+//				char[] dst = makeNextValMax(src);
+				char[] dst = makeNextValMin(src);
+
+//				System.out.println("\t(" + ((count % 2) == 0 ? 'A' : 'B') + ")next=" + String.valueOf(dst));
 				src = Arrays.copyOf(dst, dst.length);
 			}
 
@@ -58,4 +30,75 @@ public class ExpAddingGameInWonderland extends ExpBase {
 		long endTimeNs = System.nanoTime();
 		printProcessingTime(startTimeNs, endTimeNs);
 	}
+
+	private static char[] makeNextValMax(char[] src) {
+		int[] pair = new int[src.length - 1];
+		for (int i = 0; i < src.length - 1; i++) {
+			pair[i] = (src[i] - 48) + (src[i + 1] - 48);
+		}
+		int maxVal = 0;
+		int maxPos = 0;
+		boolean isOver10 = false;
+		for (int i = 0; i < pair.length; i++) {
+			if (pair[i] > maxVal) {
+				maxVal = pair[i];
+				maxPos = i;
+				if (maxVal >= 10) {
+					isOver10 = true;
+				}
+			}
+		}
+		char[] dst = new char[(isOver10 ? src.length : (src.length - 1))];
+		for (int i = 0, j = 0; i < dst.length; i++, j++) {
+			if (i == maxPos) {
+				if (isOver10) {
+					dst[i] = (char)(((int)(maxVal / 10)) + 48);
+					i++;
+					dst[i] = (char)((maxVal % 10) + 48);
+				} else {
+					dst[i] = (char)(maxVal + 48);
+				}
+				j++;
+			} else {
+				dst[i] = src[j];
+			}
+		}
+		return dst;
+	}
+
+	private static char[] makeNextValMin(char[] src) {
+		int[] pair = new int[src.length - 1];
+		for (int i = 0; i < src.length - 1; i++) {
+			pair[i] = (src[i] - 48) + (src[i + 1] - 48);
+		}
+		int minVal = 0;
+		int minPos = 0;
+		boolean isOver10 = false;
+		for (int i = 0; i < pair.length; i++) {
+			if ((i == 0) || (pair[i] < minVal)) {
+				minVal = pair[i];
+				minPos = i;
+				if (minVal >= 10) {
+					isOver10 = true;
+				}
+			}
+		}
+		char[] dst = new char[(isOver10 ? src.length : (src.length - 1))];
+		for (int i = 0, j = 0; i < dst.length; i++, j++) {
+			if (i == minPos) {
+				if (isOver10) {
+					dst[i] = (char)(((int)(minVal / 10)) + 48);
+					i++;
+					dst[i] = (char)((minVal % 10) + 48);
+				} else {
+					dst[i] = (char)(minVal + 48);
+				}
+				j++;
+			} else {
+				dst[i] = src[j];
+			}
+		}
+		return dst;
+	}
+
 }
