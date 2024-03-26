f0d0bc18210fdde382578dd9acca0f8fc94527ef
kon104 <toshiharu@MacBookAir2023.local>
2024-03-26 22:58:13 +0900

    improved source code

--- ExpExpirationDate.java
+++ ExpExpirationDate.java
@@ -13,20 +13,21 @@ public class ExpExpirationDate extends ExpBase {
 			int i1 = Integer.parseInt(str1);
 			int i2 = Integer.parseInt(str2);
 			String ans = null;
-			if (i1 <= 12) {
-				if (i2 <= 12) {
+			if ((i1 > 0) && (i1 <= 12)) {
+				if ((i2 > 0) && (i2 <= 12)) {
 					ans = "AMBIGUOUS";
 				} else {
 					ans = "MMYY";
 				}
 			} else {
-				if (i2 <= 12) {
+				if ((i2 > 0) && (i2 <= 12)) {
 					ans = "YYMM";
 				} else {
 					ans = "NA";
 				}
 			}
-			System.out.println(ans);
+			System.out.println(line + "\t" + ans);
+//			System.out.println(ans);
 		}
 		sc.close();
 
